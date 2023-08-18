7b70c02e8cdf34c433629127f6d5431aa27a7ac7
sintu-rana <95019541+sintu-rana@users.noreply.github.com>
2023-08-18 20:05:12 +0530

    ready for deployment

--- Person_Management_System/settings.py
+++ Person_Management_System/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@a^w0&zf8^3$4g6f270uo+comp4o$^7!v7vutm23&l5v&q7%-a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
